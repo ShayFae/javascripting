3b992b01cb83e8b25212a84535078205a2bdfcea
ShayFae <ifrahnoor108@gmail.com>
2021-10-26 00:54:28 +0000

    New

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+function math(length, width, height) {
+  return width * height + length;
+};
+
+console.log(math(53, 61, 67));
---
+++ objects.js
@@ -0,0 +1,14 @@
+// const pizza = {
+//   toppings: ['extra cheese', 'ground beef', 'chicken', 'extra tomato sause'],
+//   crust: 'cheese stuffed',
+//   size: 'extra large',
+//   serves: 4
+// };
+
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+}
+
+console.log(pizza);
